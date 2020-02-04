61307326470bbf279433c0e4d47407b1421e5180
Alfonso Zendejas <backdown.97@gmail.com>
2020-02-04 14:23:51 -0600

    Added models and did some changes.

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,6 +1,40 @@
+const Proyectos = require('../models/Proyectos');
 
 exports.proyectosHome = (req, res) => {
     res.render('index', {
         nombrePagina : 'Proyectos'
     });
+}
+
+exports.formularioProyecto = (req, res) => {
+    res.render('nuevoProyecto', {
+        nombrePagina : 'Nuevo Proyecto'
+    })
+}
+
+exports.nuevoProyecto = (req, res) => {
+    // Enviar a consola lo que el usuario escriba
+    // console.log(req.body);
+
+    // Validar que exista algo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+
+    if( !nombre ) {
+        errores.push({ 'texto' : 'Agrega un nombre al proyecto' })
+    }
+
+    // Si hay errores:
+    if( errores.length > 0 ) {
+        res.render('nuevoProyecto', {
+            nombrePagina : 'Nuevo Proyecto',
+            errores
+        });
+    } else {
+        // Si no hay errores, insertar en la BD
+        Proyectos.create({ nombre })
+        .then(() => console.log('Insertado correctamente'))
+        .catch(error => console.log(error));
+    }
 }
--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+// Crear la conexión a la BD
+const db = require('./config/db');
+
+// Importando el modelo
+require('./models/Proyectos');
+
+db.sync()
+    .then(() => console.log('Conectado al servidor'))
+    .catch(error => console.log(error))
 
 // Creando la app de Express
 const app = express();
@@ -13,6 +24,9 @@ app.set('view engine', 'pug');
 
 // Añadiendo la carpeta de vistas
 app.set('views', path.join(__dirname, './views'));
+
+// Habilitar bodyParser para leer datos del formulario
+app.use(bodyParser.urlencoded({ extended : true }));
 
 app.use('/', routes());
 
