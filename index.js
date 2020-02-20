b8d6f47c21ff8720e2d455c6d7632ee51ad43bb5
Alfonso Zendejas <backdown.97@gmail.com>
2020-02-20 14:44:18 -0600

    Added a helper file to perform some JSON operations.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const routes = require('./routes');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+// Helpers
+const helpers = require('./helpers');
+
 // Crear la conexión a la BD
 const db = require('./config/db');
 
@@ -24,6 +27,23 @@ app.set('view engine', 'pug');
 
 // Añadiendo la carpeta de vistas
 app.set('views', path.join(__dirname, './views'));
+
+// Pasar var dump a la app
+app.use((req, res, next) => {
+    res.locals.vardump = helpers.vardump;
+    next();
+});
+
+// Implementando Middleware
+app.use((req, res, next) => {
+    console.log('Yo soy middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Yo soy otro middleware');
+    next();
+});
 
 // Habilitar bodyParser para leer datos del formulario
 app.use(bodyParser.urlencoded({ extended : true }));
